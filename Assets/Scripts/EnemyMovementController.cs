ff1d9486b3a955dd4349dcb9d55054b4f296350b
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-09-28 11:26:21 +1300

    Fixed Enemy walk animation
    Enemy wasn't using the correct Animator component initially; now has been fixed.

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -8,21 +8,41 @@ public class EnemyMovementController : MonoBehaviour
     public NavMeshAgent enemy;
     public Transform Target;
     public Animator animator;
+    public bool isWalking;
     // Update is called once per frame
 
     void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
         Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().transform;
-        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if(isWalking == true)
+        {
+            animator.SetTrigger("walk");
+            animator.ResetTrigger("idle");
+        } else
+        {
+            animator.SetTrigger("idle");
+            animator.ResetTrigger("walk");
+        }
     }
 
     void Update()
     {
         if (Target != null)
         {
-            animator.SetTrigger("walk");
             enemy.destination = Target.position;
+            if(enemy.velocity.magnitude > 0f)
+            {
+                isWalking = true;
+            } else
+            {
+                isWalking = false;
+            }
+        }
+
         }    
-    }
 }
