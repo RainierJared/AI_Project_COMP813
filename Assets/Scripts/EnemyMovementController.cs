db6b1f499e551453d3add888a11fa0bfb2bccf37
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-10-13 20:27:34 +1300

    feat:Enemy Patrolling AI
    Incorporate Enemy Patrolling for 3 State AI.
    
    Works by having an array of points to patrol to. When enemy has a point to get to, onAPoint is set to true as to not overload the function. Once they reach their destination, Boolean is set to false and cycle is repeated.
    
    Next to implement is Alert state; will go to player's last known position if outside the viewRange and stay there for a bit until going back to Patrol state.

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -6,43 +6,112 @@ using UnityEngine.AI;
 public class EnemyMovementController : MonoBehaviour
 {
     public NavMeshAgent enemy;
-    public Transform Target;
+    public Transform playerTarget;
+    public Transform[] pointsArr;
     public Animator animator;
-    public bool isWalking;
+    public bool isWalking, onAPoint;
+    public float viewRange, attackRange;
+
+    //For 3 state AI
+
+    //Patrolling
+    public bool foundPlayer, attackPlayer;
+
+    //Layer Meshes
+    public LayerMask whatIsPlayer;
+
+    //For Patrolling
+    private int destPoint = 0;
     // Update is called once per frame
 
     void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
-        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().transform;
-    }
-
-    void FixedUpdate()
-    {
-        if(isWalking == true)
-        {
-            animator.SetTrigger("walk");
-            animator.ResetTrigger("idle");
-        } else
-        {
-            animator.SetTrigger("idle");
-            animator.ResetTrigger("walk");
-        }
+        playerTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().transform;
+        isWalking = true;
+        foundPlayer = false;
+        onAPoint = false;
+        Patrolling();
     }
 
     void Update()
     {
-        if (Target != null)
+        //For Animator
+        if (isWalking == true)
         {
-            enemy.destination = Target.position;
-            if(enemy.velocity.magnitude > 0f)
-            {
-                isWalking = true;
-            } else
-            {
-                isWalking = false;
-            }
+            animator.SetTrigger("walk");
+            animator.ResetTrigger("idle");
+        }
+        else
+        {
+            animator.SetTrigger("idle");
+            animator.ResetTrigger("walk");
         }
 
-        }    
+        foundPlayer = Physics.CheckSphere(transform.position, viewRange, whatIsPlayer);      //Flag that checks if player is within range/collides with the GameObject's sphere
+        attackPlayer = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);      //Flag that checks if player is within attack range
+
+        //Decision Tree
+        if(!foundPlayer && !attackPlayer)
+        {
+            Patrolling();
+        }
+        if (foundPlayer && !attackPlayer)
+        {
+            Alert();
+        }
+        if(foundPlayer && attackPlayer)
+        {
+            Engage();
+        }
+
+        //For if the enemy hasn't found the player and the path isn't pending
+        if (!enemy.pathPending && enemy.remainingDistance < 0.5f)
+        {
+            onAPoint = false;
+            GoToNextPoint();
+        }
+    }
+
+
+    //3 State Enemy AI
+    void Patrolling()
+    {
+        GoToNextPoint();
+    }   
+
+    void Alert()
+    {
+
+    }
+
+    void Engage()
+    {
+
+    }
+
+    private void GoToNextPoint()
+    {
+
+        if (onAPoint == false)
+        {
+            if (pointsArr.Length == 0)
+            {
+                return;
+            }
+            onAPoint = true;
+            enemy.destination = pointsArr[destPoint].position;
+            destPoint = (destPoint + 1) % pointsArr.Length;        //Modulo to cycle back to the start if end of array is reached.
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(enemy.transform.position, viewRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(enemy.transform.position, attackRange);
+    }
+
 }
